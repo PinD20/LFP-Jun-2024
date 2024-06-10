2a99658d4047e59506aa6a9a9849a07b6bc585a7
PinD20 <enrique.pinula@gmail.com>
2024-06-10 10:51:29 -0600

    Clase 5
    codificación AFD

--- Proyecto1/lexer.py
+++ Proyecto1/lexer.py
@@ -1,9 +1,28 @@
+from token import Token
+
 class Lexer():
     def __init__(self, entrada) -> None:
         self.entrada = entrada
+        self.tokens = []
 
     def isCaracterValido(self, caracter):
         return caracter in [';', '[', ']', ':', ',', '{', '}', '>']
+
+    def estado0(self, caracter, linea, columna):
+        if caracter.isalpha():
+            return 1
+        elif caracter == "-":
+            return 2
+        elif caracter == "'":
+            return 3
+        elif self.isCaracterValido(caracter):
+            return 4
+        else:
+            if ord(caracter) == 32 or ord(caracter) == 10 or ord(caracter) == 9:
+                pass
+            else:
+                print(f'ERROR LÉXICO: {caracter} ({linea}, {columna})')
+            return 0
 
     def analizar(self):
         linea = 1
@@ -11,20 +30,90 @@ class Lexer():
         lexema = ""
 
         estado = 0
+        estado_anterior = 0
 
         for caracter in self.entrada:
             if estado == 0:
+                estado = self.estado0(caracter, linea, columna)
+                if estado == 0:
+                    #Reiniciar lexema
+                    lexema = ""
+                else:
+                    lexema += caracter
+
+            elif estado == 1:
                 if caracter.isalpha():
                     lexema += caracter
-                    estado = 1
-                elif caracter == "-":
+                else:
+                    if lexema in ["nombre", "nodos", "conexiones"]:
+                        #Es un estado de aceptación, se guarda el token
+                        self.tokens.append(Token("Palabra reservada", lexema, linea, columna - len(lexema)))
+                    else:
+                        print("ERROR LÉXICO: " + lexema + " (" + str(linea) + ", " + str(columna - len(lexema)) + ")")
+
+                    lexema = ""
+
+                    #Operar como si estuviéramos en el estado 0
+                    estado = self.estado0(caracter, linea, columna)
+                    if estado != 0:
+                        lexema += caracter
+                    
+            elif estado == 2:
+                if caracter == ">":
                     lexema += caracter
-                    estado = 2
-                elif caracter == "'":
-                    lexema += caracter
-                    estado = 3
-                elif self.isCaracterValido(caracter):
-                    lexema += caracter
-                    estado = 4
+                    estado = 20
+                    estado_anterior = 2
                 else:
                     print(f'ERROR LÉXICO: {caracter} ({linea}, {columna})')
+                    estado = 0
+                    lexema = ""
+
+            elif estado == 3:
+                if caracter == "'":
+                    lexema += caracter
+                    estado = 20
+                    estado_anterior = 3
+                elif caracter == "\n":
+                    print(f'ERROR LÉXICO: {caracter} ({linea}, {columna})')
+                    estado = 0
+                    lexema = ""
+                else:
+                    estado = 3
+                    lexema += caracter
+
+            elif estado == 4:
+                #Es un estado de aceptación, se guarda el token
+                self.tokens.append(Token("Signo", lexema, linea, columna - len(lexema)))
+
+                lexema = ""
+
+                #Operar como si estuviéramos en el estado 0
+                estado = self.estado0(caracter, linea, columna)
+                if estado != 0:
+                    lexema += caracter
+                
+            elif estado == 20:
+                #Estado de aceptación
+                #Guardar el token
+                if estado_anterior == 2:
+                    self.tokens.append(Token("Asignación", lexema, linea, columna - len(lexema)))
+                elif estado_anterior == 3:
+                    self.tokens.append(Token("String", lexema, linea, columna - len(lexema)))
+                estado_anterior = 0
+
+                #Reiniciar lexema
+                lexema = ""
+
+                #Operar como si estuviéramos en el estado 0
+                estado = self.estado0(caracter, linea, columna)
+                if estado != 0:
+                    lexema += caracter
+
+            #Control de fila y columna
+            if ord(caracter) == 10:
+                linea += 1
+                columna = 1
+            elif ord(caracter) == 9:
+                columna += 4
+            else:
+                columna += 1
---
+++ Proyecto1/main.py
@@ -0,0 +1,14 @@
+from lexer import Lexer
+
+
+if __name__ == "__main__":
+    archivo = open("entrada.txt", "r", encoding="utf-8")
+    texto = archivo.read()
+
+    lexer = Lexer(texto)
+
+    lexer.analizar()
+
+    print("=================== TOKENS ===================")
+    for token in lexer.tokens:
+        print(token)
